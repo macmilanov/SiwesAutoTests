39af724b78221cca9c0150ee14656a464c179042
semenov <ssemenov@xrm.ru>
2020-07-17 00:29:13 +0500

    participant

---
+++ src/test/java/ru/xrm/selenium/toDevFeatures/RuCommonTestSteps.java
@@ -0,0 +1,9 @@
+package ru.xrm.selenium.siwesGuiTests.steps;
+
+import ru.xrm.selenium.applogic.ApplicationManager;
+
+public class RuCommonTestSteps extends ApplicationManager {
+    private ApplicationManager appManager;
+
+
+}
